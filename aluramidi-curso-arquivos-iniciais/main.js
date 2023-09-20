4962f593f51df18abebef7e00d5628e667a380ad
Lucas <112184786+Lucaswolfartm@users.noreply.github.com>
2023-09-20 08:05:29 -0300

    a

---
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -0,0 +1,9 @@
+<sction class="teclado">
+    <button class="tecla tecla_pom">Pom</button>
+
+    <button class="tecla tecla_clap">Clap</button>
+    <button class="tecla tecla_tim">Tim</button>
+
+    <button class="tecla tecla_puff">Puff</button>
+    <button class="tecla tecla_splash">Splah</button>
+</sction>
